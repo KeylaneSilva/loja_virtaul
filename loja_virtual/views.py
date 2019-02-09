b197590f99aaaf8557e4778a2461d1e43d0e1eff
ggalfa <gleisoncosta705@gmail.com>
2019-02-08 23:13:57 -0300

    Criando templates de about e contact, adicionado ao urls.py e criando as respectivas views passando o title e conteudo das paginas por context com um dicionário.

--- loja_virtual/views.py
+++ loja_virtual/views.py
@@ -2,4 +2,25 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home_page.html', {})
+    contex = {
+        "title": "Página Principal",
+        "content": "Bem-vindo a página principal"
+    }
+    return render(request, 'home_page.html', contex)
+
+
+def about_page(request):
+    context = {
+        "title": "Página Sobre",
+        "content": "Bem-vindo a página sobre."
+    }
+    return render(request, 'about/view.html', context)
+
+
+def contact_page(request):
+    context = {
+        "title": "Página de contato",
+        "content": "Bem-vindo a página de contato"
+    }
+    return render(request, 'contact/view.html', context)
+    
